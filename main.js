d668aa696a8acd7f330bbc08874f3978f58a8e17
Denis Simplicio Pereira <denis_simplicio@hotmail.com>
2024-04-21 11:28:13 -0300

    ajuste no alert nota media

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
-const notaMinima = parseFloat(prompt('Digite a nota minima: '));
+const notaMinima = parseFloat(prompt('Digite a nota média: '));
 
 let linhas = '';
 
